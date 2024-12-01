d284efa661cd6f4cc4cdc4eea05bffa2f2089035
Henry <77937076+yung-turabian@users.noreply.github.com>
2024-12-01 17:47:22 -0500

    Update conf_env.sh

--- build_tools/conf_env.sh
+++ build_tools/conf_env.sh
@@ -5,8 +5,8 @@ OS=$(uname)
 if [[ "$OS" == "Darwin" ]]; then
   echo "Installing GHC, Happy, and Alex on macOS..."
 
-  brew install ghc stack
-  stack install happy alex readline
+  brew install ghc cabal-install
+  cabal install happy alex readline
 
 elif [[ "$OS" == "Linux" ]]; then
   echo "Installing GHC, Happy, and Alex on Linux..."
